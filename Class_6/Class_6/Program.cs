c067038344e230460317b9e79c5db81698b03fb3
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2023-11-27 18:11:25 -0400

    Add Task 6 version 1

---
+++ Class_6/Class_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Class_6.Class_6;
+using System;
+using System.Collections.Generic;
+
+namespace Class_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicles = new List<Vehicle>
+            {
+                new Car("Toyota", "Corolla"),
+                new Motorcycle("Harley-Davidson", "Street 750"),
+                new Helicopter("Bell", "777")
+            };
+
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Start();
+                vehicle.Stop();
+            }
+        }
+    }
+}
---
+++ Class_6/Class_6/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_6
+{
+    public abstract class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public string Type { get; protected set; }
+
+        protected Vehicle(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+
+        public override string ToString() 
+        { 
+            return $"{Type}: {Make} - {Model}"; 
+        }
+    }
+}
