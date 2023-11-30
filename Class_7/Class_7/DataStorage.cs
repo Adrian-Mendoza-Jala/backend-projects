0dee6935435447b20dde3fa7307c265e589433d4
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2023-11-29 23:30:55 -0400

    Class 7 Data Storage Done

---
+++ Class_7/Class_7/DataStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Class_7
+{
+
+    public class DataStorage<T> where T : new()
+    {
+        private List<T> storage = new List<T>();
+
+        public void Add(T item)
+        {
+            storage.Add(item);
+        }
+
+        public bool Remove(T item)
+        {
+            return storage.Remove(item);
+        }
+
+        public T Retrieve(int index)
+        {
+            return storage[index];
+        }
+
+        public void DisplayAll()
+        {
+            foreach (T item in storage)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Class_7/Class_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class_7
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataStorage<int> intStorage = new DataStorage<int>();
+            intStorage.Add(10);
+            intStorage.Add(20);
+            intStorage.Add(30);
+            intStorage.Add(40);
+            intStorage.Add(50);
+            intStorage.Add(100);
+            Console.WriteLine("Int Storage:");
+            intStorage.DisplayAll();
+
+            DataStorage<SimpleClass> classStorage = new DataStorage<SimpleClass>();
+            classStorage.Add(new SimpleClass());
+            classStorage.Add(new SimpleClass());
+            Console.WriteLine("\nClass Storage:");
+            classStorage.DisplayAll();
+        }
+    }
+}
