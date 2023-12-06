88f59d9171fd14c6e60313b56d3d201ef46ee499
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2023-12-06 00:12:04 -0400

    Start Skill Mastery API with Skill -  Get All, Get By ID and Post

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Backend Projects
 
 
-## ✅ Class 1 - Language Infrastructures Comparison 
-## ✅ Class 2 - Console App | Task Manager  
-## ✅ Class 3 - Console App | Input Commands  
-## ✅ Class 4 - Console App | Add Calculator 
-## ✅ Class 5 - Console App | Quiz Game
-## ✅ Class 6 - Console App | Vehicle Structure 
-## ✅ Class 7 - Console App | Generic Class
-## ✅ Class 8 - Console App | Exceptions
-## ✅ Class 9 - Research
-## ✅ Class 10 - Design an Application for Skill Mastery
+### ✅ Class 1 - Language Infrastructures Comparison 
+### ✅ Class 2 - Console App | Task Manager  
+### ✅ Class 3 - Console App | Input Commands  
+### ✅ Class 4 - Console App | Add Calculator 
+### ✅ Class 5 - Console App | Quiz Game
+### ✅ Class 6 - Console App | Vehicle Structure 
+### ✅ Class 7 - Console App | Generic Class
+### ✅ Class 8 - Console App | Exceptions
+### ✅ Class 9 - Research
+### ✅ Class 10 - Design an Application for Skill Mastery
+___
+## SkillMasteryAPI 
+-  ✅  Skill (GetAll, GetById, Post) Connection 
---
+++ SkillMasteryAPI/SkillMasteryAPI/Controllers/SkillsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillMasteryAPI.Models;
+using SkillMasteryAPI.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkillMasteryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly ISkillService _skillService;
+
+        public SkillsController(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills()
+        {
+            var skills = await _skillService.GetAllSkillsAsync();
+            return Ok(skills);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Skill>> PostSkill(Skill skill)
+        {
+            await _skillService.AddSkillAsync(skill);
+            return CreatedAtAction(nameof(GetSkill), new { id = skill.Id }, skill);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>> GetSkill(int id)
+        {
+            var skill = await _skillService.GetSkillByIdAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            return skill;
+        }
+    }
+}
