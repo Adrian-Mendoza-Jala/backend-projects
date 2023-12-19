898755c16e265df06ea5ef9dc37443f49b002452
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2023-12-19 01:31:52 -0400

    Add Struct, Record and Class Examples

---
+++ struct-record-class/struct-record-class/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace struct_record_class
+{
+    public class Employee : IWorker
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+
+        public Employee(string name, string department)
+        {
+
+            Name = name;
+            Department = department;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Department: {Department}");
+        }
+
+        public void Work() { Console.WriteLine("Working"); }
+    }
+}
---
+++ struct-record-class/struct-record-class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using struct_record_class;
+class Program
+{
+    static void Main()
+    {
+        /**
+         * Class
+         */
+        Employee employee = new Employee("Juan", "Tech");
+        employee.Name = "Ana"; // Change allowed
+
+        Employee employee1 = new Employee("Carlos", "Tech");
+        Employee employee2 = employee1; // Both point to the same object
+
+        /**
+         * Struct
+         */
+        Point p1 = new Point(1, 2);
+        // p1.X = 3; // This would be a compilation error
+
+        /**
+        * Record
+        */
+        User user1 = new User("Ana", "ana@email.com");
+        User user2 = new User("Ana", "ana@email.com");
+        bool areEqual = user1 == user2; // True
+
+        User user3 = new User("Ana", "email");
+        User user4 = user3 with { Email = "newEmail" };
+
+        User user = new User("Ana", "email");
+        // user.Name = "Carlos"; // Compilation error
+
+    }
+}
