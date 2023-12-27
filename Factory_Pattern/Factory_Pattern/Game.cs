675179ae6441a39d4b4dcc88191b1140cde8446c
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2023-12-27 13:33:33 -0400

    Factory Pattern v1

---
+++ Factory_Pattern/Factory_Pattern/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern
+{
+    public class Game
+    {
+        private Random random = new Random();
+
+        public IEnemy SpawnEnemy()
+        {
+            double randomNumber = random.NextDouble();
+            if (randomNumber > 0.66)
+            {
+                return new Koopa();
+            }
+            else if (randomNumber > 0.33)
+            {
+                return new Goomba();
+            }
+            else
+            {
+                return new Boo();
+            }
+        }
+
+        public void GameLogic()
+        {
+            // ... more code above
+
+            if (ShouldSpawnEnemy())
+            {
+                IEnemy enemy = SpawnEnemy();
+                enemy.Attack();
+            }
+
+            // ... more code below, use enemy
+        }
+
+        private bool ShouldSpawnEnemy()
+        {
+            // Logic to determine if an enemy should spawn
+            return true;
+        }
+    }
+}
---
+++ Factory_Pattern/Factory_Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factory_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            // Simulate enemy spawning and attacking 10 times
+            for (int i = 0; i < 10; i++)
+            {
+                IEnemy enemy = game.SpawnEnemy();
+                enemy.Attack();
+            }
+        }
+    }
+}
